5e5d80c1407b51f21e6e0f46b9b88de442e66d76
Viper <momo2010sofia@gmail.com>
2025-01-12 18:43:56 +0000

    api

--- include/Main.hpp
+++ include/Main.hpp
@@ -18,9 +18,37 @@ public:
    static VP_DialogLayer* createWithObjects(CCArray* objects, int background) {
         return reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(objects,background));
    };
+
+   static VP_DialogLayer* createWithObjects(CCArray* objects, std::string background,std::function<void()> m_callback) {
+     VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(objects,2));
+     x->setBackground(background);
+     x->addCallbackCustom(m_callback);
+     return x;
+   };
+   static VP_DialogLayer* createWithObjects(CCArray* objects, std::string background) {
+     VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(objects,2));
+     x->setBackground(background);
+     return x;
+   };
+
    static VP_DialogLayer* create(DialogObject* object, int background) {
         return reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,background));
    };
+
+   static VP_DialogLayer* create(DialogObject* object, std::string background) {
+       VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,2));
+       x->setBackground(background);
+       return x;
+   };
+
+   static VP_DialogLayer* create(DialogObject* object, std::string background,std::function<void()> m_callback) {
+       VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,2));
+       x->addCallbackCustom(m_callback);
+       x->setBackground(background);
+       return x;
+   };
+
+
    static VP_DialogLayer* createWithJson(matjson::Value json,std::function<void()> m_callback) {
           std::pair<CCArray*, int> jsonread = Viper_funnyutils::readjsonData(json);
           auto l = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(jsonread.first,jsonread.second));
@@ -32,6 +60,7 @@ public:
           auto l = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(jsonread.first,jsonread.second));
           return l;
    };
+   
    static VP_DialogLayer* create(matjson::Value json) {
           std::pair<CCArray*, int> jsonread = Viper_funnyutils::readjsonData(json);
           auto l = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(jsonread.first,jsonread.second));
@@ -62,8 +91,28 @@ public:
          return mass;
    };
 
+   static VP_DialogLayer* createWithCallback(DialogObject* object, std::string background, std::function<void()> m_callback) {
+       VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,2));
+       x->setBackground(background);
+       x->addCallbackCustom(m_callback);
+       return x;
+   };
+
    static VP_DialogLayer* createDialogLayer(DialogObject* object,CCArray* objects, int background) {
         return reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createDialogLayer(object,objects,background));
+   };
+
+   static VP_DialogLayer* createDialogLayer(DialogObject* object,CCArray* objects, std::string background) {
+        VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createDialogLayer(object,objects,2));
+        x->setBackground(background);
+        return x;
+   };
+
+      static VP_DialogLayer* createDialogLayer(DialogObject* object,CCArray* objects, std::string background,std::function<void()> m_callback) {
+        VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createDialogLayer(object,objects,2));
+        x->setBackground(background);
+        x->addCallbackCustom(m_callback);
+        return x;
    };
 
    static VP_DialogLayer* createDialogLayer(DialogObject* object, int background, std::function<void()> m_callback) {
@@ -87,9 +136,26 @@ public:
    static VP_DialogLayer* createDialogLayer(DialogObject* object, int background) {
         return reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,background));
    };
+    static VP_DialogLayer* createDialogLayer(DialogObject* object, std::string background) {
+       VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,2));
+       x->setBackground(background);
+       return x;
+   };
+
+   static VP_DialogLayer* createDialogLayer(DialogObject* object, std::string background, std::function<void()> m_callback) {
+       VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,2));
+       x->setBackground(background);
+       x->addCallbackCustom(m_callback);
+       return x;
+   };
    
     static VP_DialogLayer* createDialogLayer(CCArray* objects, int background) {
         return reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::createWithObjects(objects,background));
+   };
+    static VP_DialogLayer* createDialogLayer(DialogObject* object, std::string background) {
+       VP_DialogLayer* x = reinterpret_cast<VP_DialogLayer*>(VP_DialogLayer::create(object,2));
+       x->setBackground(background);
+       return x;
    };
     // som
    static VP_DialogObject* create(gd::string character,
